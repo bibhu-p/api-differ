f63107bcb38443e61b882b969d9efe29438142cf
Bibhu Prasad <bibhup.behera@nettantra.net>
2025-11-20 14:40:05 +0530

    Add products controller and modify users controller

--- examples/users.controller.ts
+++ examples/users.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, Query } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Patch, Param, Body, Query } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
     @Get()
-    findAll(@Query('page') page: number) {
+    findAll(@Query('page') page: number, @Query('limit') limit: number) {
         return [];
     }
 
@@ -22,8 +22,28 @@ export class UsersController {
         return {};
     }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
+    // Removed Delete endpoint
+
+    @Patch(':id/status')
+    updateStatus(@Param('id') id: string, @Body('status') status: string) {
+        return {};
+    }
+}
+
+@Controller('products')
+export class ProductsController {
+    @Get()
+    findAll() {
+        return [];
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return {};
+    }
+
+    @Post()
+    create(@Body() createProductDto: any) {
         return {};
     }
 }
